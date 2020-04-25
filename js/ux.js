55b72274a874496d1549dbdb7efd54eb039ffd7b
Pedro D <josepedrodiaz@gmail.com>
2020-04-24 21:37:50 -0300

    Ignore XMl files and move some javascript to external file

---
+++ js/ux.js
@@ -0,0 +1,4 @@
+console.log('%cMade with %c♥ %cby Pedro',
+            'color:#147f30;font-size:16px;font-family:Lucida Console, Monaco, monospace',
+            'color:#ce2184;font-size:18px;font-family:Lucida Console, Monaco, monospace',
+            'color:#228baf;font-size:16px;font-family:Lucida Console, Monaco, monospace');
